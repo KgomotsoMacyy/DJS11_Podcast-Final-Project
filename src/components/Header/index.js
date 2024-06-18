e4d27a28b9887fbf05a86456dbaee5d3b199e6ff
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-06-18 15:20:28 +0200

    "Refactor App component to use Header component, remove unnecessary code and add styles to Header"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import "./styles.css";
 
 function Header() {
-    return <div className="nav">
-        Header
-    </div>;
+    return (
+        <div className="nav">
+            Header
+        </div>
+    )
 }
 
 export default Header;
