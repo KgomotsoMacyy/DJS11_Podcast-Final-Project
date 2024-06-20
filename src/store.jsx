a67079e9735d4c65788cf96869c708eb999088af
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-06-20 10:50:13 +0200

    Updated store configuration to use configureStore from @reduxjs/toolkit and added reducers for user, podcast, and episode slices.

--- src/store.jsx
+++ src/store.jsx
@@ -1,4 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import { useReducer } from "react";
-import podcastReducer from ".sli"
+import userReducer from "./slices/userSlice";
+import podcastReducer from "./slices/podcastSlice";
+import episodeReducer from "./slices/episodeSlice";
+
+export default configureStore({
+    reducer: {
+        user: userReducer,
+        podcast: podcastReducer,
+        episode: episodeReducer,
+    },
+})
