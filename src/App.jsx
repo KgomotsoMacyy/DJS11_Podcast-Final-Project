c7725fc44c01abe0c491677a9477bcd4656c8f0a
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-06-20 11:21:42 +0200

    Added react-router-dom dependency and implemented routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import './index.css'
 import Header from './components/Header/header'
 
 function App() {
-  return 
+  return (
     <div className="App">
-      <Header />
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignInSignUp />} />
+          <Route element={<PrivateRoutes />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/podcasts" element={<Podcasts />} />
+          <Route path="/create-podcast" element={<CreatePodcast />} />
+          <Route path="/podcast/:podcastId" element={<PodcastDetails />} />
+          <Route
+            path="/podcast/:podcastId/create-episode"
+            element={<CreateEpisode />}
+          />
+        </Routes>
+      </Router>
     </div>
+  );
 }
 export default App;
